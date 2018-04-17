a2e2cfe0aa4b76fe6b2e987e4037df6f693e5e8f
Fabio Trilho Pereira <fabiotrilhopereira@gmail.com>
2018-04-17 19:35:12 +0100

    Final PokemonJSON commit
    PokemonJSON Pratice Set completed

--- PokemonJSON/app/src/main/java/udacity/pokemon/HttpHandler.java
+++ PokemonJSON/app/src/main/java/udacity/pokemon/HttpHandler.java
@@ -15,6 +15,7 @@ package udacity.pokemon;
  * See the License for the specific language governing permissions and
  * limitations under the License.
  */
+
 import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStream;
@@ -24,54 +25,54 @@ import java.net.URL;
 
 public class HttpHandler {
 
-    public HttpHandler() {
+  public HttpHandler() {
+  }
+
+  public String makeHttpRequest(URL url) throws IOException {
+    String jsonResponse = "";
+    HttpURLConnection urlConnection = null;
+    InputStream inputStream = null;
+    try {
+      urlConnection = (HttpURLConnection) url.openConnection();
+      urlConnection.setRequestMethod("GET");
+      urlConnection.setReadTimeout(10000);
+      urlConnection.setConnectTimeout(15000);
+      urlConnection.connect();
+      inputStream = urlConnection.getInputStream();
+      jsonResponse = convertStreamToString(inputStream);
+
+    } catch (IOException e) {
+      //handle the exception
+    } finally {
+      if (urlConnection != null) {
+        urlConnection.disconnect();
+      }
+      if (inputStream != null) {
+        inputStream.close();
+      }
+      return jsonResponse;
+    }
+  }
+
+  private String convertStreamToString(InputStream is) {
+    BufferedReader reader = new BufferedReader(new InputStreamReader(is));
+    StringBuilder sb = new StringBuilder();
+
+    String line;
+    try {
+      while ((line = reader.readLine()) != null) {
+        sb.append(line).append('\n');
+      }
+    } catch (IOException e) {
+      e.printStackTrace();
+    } finally {
+      try {
+        is.close();
+      } catch (IOException e) {
+        e.printStackTrace();
+      }
     }
 
-    public String makeHttpRequest(URL url) throws IOException {
-        String jsonResponse = "";
-        HttpURLConnection urlConnection = null;
-        InputStream inputStream = null;
-        try {
-            urlConnection = (HttpURLConnection) url.openConnection();
-            urlConnection.setRequestMethod("GET");
-            urlConnection.setReadTimeout(10000);
-            urlConnection.setConnectTimeout(15000);
-            urlConnection.connect();
-            inputStream = urlConnection.getInputStream();
-            jsonResponse = convertStreamToString(inputStream);
-
-        } catch (IOException e) {
-            //handle the exception
-        } finally {
-            if (urlConnection != null) {
-                urlConnection.disconnect();
-            }
-            if (inputStream != null) {
-                inputStream.close();
-            }
-            return jsonResponse;
-        }
-    }
-
-    private String convertStreamToString(InputStream is) {
-        BufferedReader reader = new BufferedReader(new InputStreamReader(is));
-        StringBuilder sb = new StringBuilder();
-
-        String line;
-        try {
-            while ((line = reader.readLine()) != null) {
-                sb.append(line).append('\n');
-            }
-        } catch (IOException e) {
-            e.printStackTrace();
-        } finally {
-            try {
-                is.close();
-            } catch (IOException e) {
-                e.printStackTrace();
-            }
-        }
-
-        return sb.toString();
-    }
+    return sb.toString();
+  }
 }
