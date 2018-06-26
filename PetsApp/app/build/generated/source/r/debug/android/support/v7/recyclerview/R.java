6f73bdc271d544089fac231406e797649129a4c6
Fabio Trilho Pereira <fabiotrilhopereira@gmail.com>
2018-06-26 13:02:02 +0100

    Finished state of PetsApp

--- PetsApp/app/build/generated/source/r/debug/android/support/v7/recyclerview/R.java
+++ PetsApp/app/build/generated/source/r/debug/android/support/v7/recyclerview/R.java
@@ -19,7 +19,7 @@ public final class R {
         public static final int item_touch_helper_swipe_escape_velocity = 0x7f060074;
     }
     public static final class id {
-        public static final int item_touch_helper_previous_elevation = 0x7f080048;
+        public static final int item_touch_helper_previous_elevation = 0x7f08004c;
     }
     public static final class styleable {
         public static final int[] RecyclerView = { 0x010100c4, 0x010100f1, 0x7f03009a, 0x7f0300d2, 0x7f0300e0, 0x7f0300e6 };
